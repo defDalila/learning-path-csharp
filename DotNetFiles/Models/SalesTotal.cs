7c29e86f78be53972681acfda0fd7adfd4c39056
defdalila <defdalila@outlook.com>
2025-04-03 05:47:27 -0300

    data updt

---
+++ DotNetFiles/Models/SalesTotal.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetFiles.Models;
+
+public class SalesTotal
+{
+    public double Total { get; set; }
+}
